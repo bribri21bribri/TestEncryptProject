619530a00ae8f83614ad6d52a4a96e1aca1f255b
root <root@localhost>
2012-12-31 19:00:01 -0400

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+
+export const routes: Routes = [
+    {component: HelloComponent, path: ""}
+];
